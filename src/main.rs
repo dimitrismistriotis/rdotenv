7ca8015af58a4880bf77bd7f16a0a82b4bf9cc15
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2023-04-17 09:13:33 +0100

    Exec module to override process.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    let joined_args = args.join(" ");
+    println!("{}", joined_args);
+
+    let err = exec::Command::new("echo")
+        .arg("hello").arg("world")
+        .exec();
+    println!("Error: {}", err);
 }
